6599723481c495c2cb2cc3bba2504bcd55e439d6
~jusdepatate <yaume@ntymail.com>
2018-11-12 19:12:15 +0100

    Add comments

--- info.sh
+++ info.sh
@@ -12,91 +12,155 @@
 # License : CC-BY-NC "Jus de Patate, 2018"
 
 KERNELNAME="$(uname -s)"
+# set variable KERNELNAME to the output of command 'uname -s'
 OS="$(uname -o)"
+# set variable OS to the output of command 'uname -o'
 KERNEL="$(uname -r)"
+# set variable KERNEL to the output of command 'uname -r'
 PKGARCH="$(dpkg --print-architecture)"
+# set variable PKGARCH to the output of command 'dpkg --print-architecture'
 USER="$(whoami)"
+# set variable USER to the output of commande 'whoami'
 
 FIRST="$USER@"
+# set variable FIRST (first line of output) to the variable USER and @
 
 if [ "$OS" = "Android" ];then
+# If user's os is Android (Termux)
         # --- FOR ANDROID (Termux) ---
         ISPNAME="$(getprop gsm.sim.operator.alpha)"
+        # set variable ISPNAME to the output of command 'getprop gsm.sim.operator.alpha', it's output is the operator name of the user (getprop is very useful on Termux)
         ISPCOUNTRY="$(getprop gsm.operator.iso-country)"
+        # set variable ISPCOUNTRY to the output of command 'getprop gsm.operator.iso-country', it's output is the iso code of the user's operator
         ANDVERSION="$(getprop ro.build.version.release)"
+        # set variable ANDVERSION to the output of command 'uname -r', it's output is the version of Android
 
         FIRST+="$(getprop ro.product.manufacturer)"
+        # add to the variable FIRST (first line of output) the output of command 'getprop ro.product.manufacturer', it's output is the user's phone manufacturer (Huawei, Samsung, ...)
         FIRST+=" $(getprop ro.product.model)"
+        # add to the variable FIRST (''                  ) the output of command 'getprop ro.product.model', it's output is the user's phone model
 
         SECOND="$OS"
+        # set variable SECOND (second line of output) to the output of variable OS (Android in every case in this if)
         SECOND+=" $ANDVERSION"
+        # add to the variable SECOND the variable ANDVERSION
         SECOND+=" ($KERNELNAME $KERNEL)"
+        # add to the variable SECOND the variable KERNELNAME and KERNEL
 
         BONUS1="Mobile ISP : $ISPNAME ($ISPCOUNTRY)"
+        # set variable BONUS1 to "Mobile ISP : " and variable ISPNAME and ISPCOUNTRY
 else
+# else :troll:
         # --- FOR ANYTHING ELSE (including cygwin) ---
         FIRST+="$(uname -n)"
+        # add to variable FIRST the output of command 'uname -n' (machine name)
 
         case "$OSTYPE" in
+        # if env variable OSTYPE is equal to
                 solaris*) OS="Solaris" ;;
-                darwin*)  OS="MAC OS X" ;;
+                # solaris* then set varible OS to "Solaris"
+                darwin*)  OS="Mac OS X" ;;
+                # darwin* then set varible OS to "Mac OS X"
                 linux*)   OS="GNU/Linux" ;;
+                # linux* then set varible OS to "GNU/Linux" (yes, thats how we should call it)
                 bsd*)     OS="BSD" ;;
+                # bsd* then set varible OS to "BSD"
                 FreeBSD)  OS="FreeBSD" ;;
+                # FreeBSD then set varible OS to "FreeBSD"
                 msys*)    OS="Windows" ;;
+                # msys* then set varible OS to "Windows" (not sure this can happen
                 cygwin*)  OS="Windows" ;;
+                # cygwin* then set varible OS to "Windows"
                 *)        OS="$OSTYPE" ;;
+                # anything else then set varible OS to env var OSTYPE
         esac
+        # end of case
         if [ "$OS" = "GNU/Linux" ]; then
+        # if variable OS is equal to GNU/Linux
                 if [ `which yum` ]; then
+                # and if which yum is true (package exist)
                         OS="Red Hat"
+                        # set variable OS to Red Hat
                 elif [ `which apt` ]; then
+                # or if which apt is true (package exist)
                         source /etc/os-release
+                        # take variables from /etc/os-release
                         source /etc/lsb-release
+                        # and variables from /etc/lsb-release
                         OS="$DISTRIB_ID ${DISTRIB_CODENAME^} $DISTRIB_RELEASE"
+                        # set variable OS to DISTRIB_ID (ex: Ubuntu) and DISTRIB_CODENAME with first letter uppercased and DISTRIB_RELEASE
                 elif [ `which apk` ]; then
+                # or if which apk is positive (package exists)
                         OS="Alpine Linux"
+                        # set variable OS to "Alpine Linux"
                 else
+                # NO ! I WON´T DO THIS JOKE TWO TIMES IN ONE FILE
                         Aba="dakor"
+                        # random bullshit
                 fi
+                # end of if
         fi
+        # end of if
         SECOND="$OS"
+        # set variable SECOND (second line of output) to variable OS
         SECOND+=" ($KERNELNAME $KERNEL)"
+        # add to variable SECOND the variable KERNELNAME and KERNEL
 fi
+# end of if
 
 THIRD="Package Arch: $PKGARCH"
+# set variable THIRD to variable PKGARCH
 
 FOURTH="Shell:"
+# set variable FOURTH to "Shell:"
 if [ $(echo $SHELL || grep "zsh") ]; then
+# if env var SHELL contains "zsh"
         SH="$(zsh --version | grep -o 'zsh [0-9]\.[0-9]\.[0-9]')"
+        # set SH variable to output of "zsh --version" (modified using grep)
         FOURTH+=" $SH"
+        # add to variable FOURTH variable SH
 elif [ $(echo $SHELL || grep "bash") ]; then
+# or if env var contains bash
         SH="$(bash --version | head -1 | cut -d ' ' -f 4)"
+        # set SH variable to output of "bash --version" (modified using head and cut)
         # https://askubuntu.com/a/1008422
         FOURTH+=" bash $SH"
-elif [ $(echo $SHELL || grep "sh") ]; then
+        # add to variable FOURTH "bash" and variable SH
+elif [ $(echo $SHELL || grep "/bin/sh") ]; then
+# or if env var contains /bin/sh
         FOURTH+=" sh"
+        # add to variable FOURTH "sh"
 else
+# No.
         FOURTH+=" $SHELL"
+        # add to variable FOURTH the variable SHELL
 fi
 
 FIFTH="Public IP(v4): "
+# set variable FIFTH "Public IP(v4): "
 FIFTH+=" $(curl -s --max-time 10 https://v4.ident.me)"
+# connect to v4.ident.me with timeout of 10 seconds and put output into variable FIFTH
 FIFTH+=" ($(curl -s --max-time 10 ifconfig.io/country_code))"
+# connect to ifconfig.io/country_code with timeout of 10 seconds and put output into variable FIFTH
 
 if [ "$(curl -s --max-time 10 https://v6.ident.me/)" ]; then
+# if i can connect to v6.ident.me with 10s of timeout
         FIFTH+="\nPublic IP(v6): $(curl -s --max-time 10 https://v6.ident.me/) ($(curl -s --max-time 10 ifconfig.io/country_code))"
+        # add a line to variable FIFTH containing result of v6.ident.me and ifconfig.io with 10s of timeout for both
 fi
 
 SIXTH="Local IP: "
+# set variable SIXTH to "Local IP: "
 
 # Thanks to Dryusdan :)
 # Tested on Debian 9.5 and 9.6 and Termux
 SIXTH+="$(ip route get 2 | awk '{print $7;exit}')"
+# get the output of 'ip route get 2', do some tricks with awk and put output into variable SIXTH
 
 # --- ECHO ---
 
 echo $FIRST
+# do I really need to explain 'echo' ?
 # First : user@host (or phone man + phone mod)
 
 echo $SECOND
@@ -108,7 +172,8 @@ echo $THIRD
 echo $FOURTH
 # Fourth : Shell + version (only for bash and zsh)
 
-echo $FIFTH
+echo -e $FIFTH
+# -e because it supports \n (new line)
 # Fifth : Public IP
 
 echo $SIXTH
